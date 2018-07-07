b6f5578e1aa9d072e69a05eaa466f184b815c6a3
alisafari <alisafar1212@gmail.com>
2018-07-07 13:26:10 +0430

    Add Initial packages

---
+++ src/main/java/ir/nimbo2/nimroo/cooler/App.java
@@ -0,0 +1,13 @@
+package ir.nimbo2.nimroo.cooler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+    }
+}
---
+++ src/test/java/ir/nimbo2/nimroo/cooler/AppTest.java
@@ -0,0 +1,20 @@
+package ir.nimbo2.nimroo.cooler;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
